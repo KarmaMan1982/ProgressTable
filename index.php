1b2ed96ad1e54f622d1da8c7cbcd05b65f247aa5
KarmaMan1982 <markuseberherr@googlemail.com>
2017-12-08 14:00:19 +0100

    Old Firefox - Support

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <html>
     <head>
         <title>Progress-Table</title>
-        <?php if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== false) { ?>
+        <?php
+        $oldFirefox = false;
+        $browser = get_browser(null, true);
+        if($browser['browser'] == 'Firefox' && $browser['majorver'] <= 10){ $oldFirefox = true; }        
+        ?>
+        <?php if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== false || $oldFirefox == true) { ?>
         <!--
         <link href="style/reset.css" rel="stylesheet" type="text/css"/>
         -->
@@ -14,7 +19,7 @@
         <link href="lib/jquery-ui-themes-1.12.1/themes/excite-bike/theme.css" rel="stylesheet" type="text/css"/>
         <?php } ?> 
         <link href="style/ProgressTable.css" rel="stylesheet" type="text/css"/>        
-        <?php if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== false) { ?>
+        <?php if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6.') !== false || $oldFirefox == true) { ?>
         <script src="lib/jquery-ui-1.9.2.custom/js/jquery-1.8.3.js" type="text/javascript"></script> 
         <script src="lib/jquery-ui-1.9.2.custom/js/jquery-ui-1.9.2.custom.js" type="text/javascript"></script>
         <script src="js/indexIE6.js" type="text/javascript"></script>          
